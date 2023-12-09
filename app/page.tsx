0f543438e0e4da1da73415c2cd566dc623eb790c
Nahuel Luca <nahuelluca20@gmail.com>
2023-12-09 18:00:04 -0300

    page "/" layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 import LayoutContainer from "@/components/layout-container";
+import NextAuthImage from "@/assets/next-auth.jpg";
+import {Button} from "@/components/ui/button";
 
 export default function page() {
   return (
-    <LayoutContainer className="flex justify-center">
-      <h1 className="text-2xl font-extrabold">Hi!</h1>
+    <LayoutContainer className="flex-col gap-10 min-h-screen items-center flex flex-wrap  justify-center">
+      <h1 className="text-4xl md:text-6xl max-w-[600px] text-center font-extrabold">
+        Welcome to a small example of NextJS 14 + AuthJS
+      </h1>
+      <div className="flex gap-5">
+        <Button asChild>
+          <Link href={"/public"}>Public Route</Link>
+        </Button>
+
+        <Button asChild variant={"destructive"}>
+          <Link href={"/protected"}>Privated Route</Link>
+        </Button>
+      </div>
+      <Image
+        alt="Nextjs + Authjs / NextAuth"
+        className="w-full lg:w-[600px] rounded-xl"
+        src={NextAuthImage}
+      />
     </LayoutContainer>
   );
 }
